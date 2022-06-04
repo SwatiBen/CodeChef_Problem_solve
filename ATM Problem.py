d164d8047f101d7ab441e07bdf5387b07ce665fb
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2022-06-04 12:30:40 +0530

    Update ATM Problem.py

--- ATM Problem.py
+++ ATM Problem.py
@@ -1,8 +1,11 @@
-x, y = map(float, input().split()) # space is a must after ',' and before 'input'
+x, y = map(float, input().split())
+
 if(x%5==0 and x<y):
     d=y-(x+0.5)
     print("%.2f"%d)
+    
 elif(x%5!=0):
     print("%.2f"%y)
+    
 else:
     print("%.2f"%y)
